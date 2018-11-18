4d6059e63354f32c2afece686fa90ada0422a2fe
Chris Meyers <cm.02.93@gmail.com>
2018-11-18 18:49:40 -0500

    banner image border now lines up with content section title border.

--- web/src/components/Navigation.vue
+++ web/src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <div class="nav">
     <div id="full-menu">
       <div class="side-nav-logo">
-        <router-link class="nav-link" to="/"><a id="banner-small"><img src="@/assets/images/logos/meyers-logo-green.svg" alt="Chris Meyers. Developer, Tech enthusiast." class="banner-img" title="Home"></a></router-link>
+        <router-link class="nav-link banner" to="/"><img src="@/assets/images/logos/meyers-logo-green.svg" alt="Chris Meyers. Developer, Tech enthusiast." class="banner-img" title="Home"></router-link>
       </div>
       <ul class="side-nav-items">
         <li><router-link class="nav-link" :class="{ 'nav-selected': path == '' }" to="/">About</router-link></li>
@@ -49,99 +49,10 @@ export default {
   color: #5bb75b !important;
 }
 
-#banner {
-  float: left;
-  height: 60px;
-}
-
-#banner-small img {
+.banner img {
   width:100%;
   display: inline-block;
-}
-
-.banner-img-small {
-  float: left;
-  height: 20px;
-  width: 70px;
-}
-
-#banner-icon {
-  float: right;
-  margin-right: 0px;
-}
-
-.bannerBar-size {
-  height: 60px;
-}
-
-.section-title-txt {
-  margin-top: 24px;
-}
-
-#section-title-menu {
-  background-color: #FFFFFF;
-  display: table-cell;
-  color: white;
-  font-size: x-large;
-  font-family: 'Open Sans', sans-serif;
-  text-align: center;
-}
-#section-title-menu:hover {
-  cursor: pointer;
-}
-
-.section-title-small-txt {
-  vertical-align: middle;
-  line-height: 50px;
-  text-align: center;
-  border-bottom: 1px solid #DCDCDC;
-}
-.section-title-small-txt-last {
-  vertical-align: middle;
-  line-height: 50px;
-  text-align: center;
-}
-
-#menu-dropdown {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-#menu-items {
-  padding: 0px;
-  list-style: none;
-  display: none;
-  position: absolute;
-  float: left; clear: right;
-  left: 0px;
-  right: 0px;
-  background: #FFFFFF;
-  border-top: 1px solid rgba(0, 0, 0, 0.1);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-  z-index: 1;
-}
-
-#menu-items li {
-  height: 50px;
-  padding: 0px;
-}
-
-#menu-items li a {
-  display: block;
-  height: 100%;
-  width: 100%;
-}
-
-#menu-toggle {
-  width: 25px;
-  height: 27px;
-  margin-top: 14px;
-}
-
-#menu-click {
-  height: 60px;
-  outline: none;
+  padding-top: 10px;
 }
 
 .nav {
@@ -161,19 +72,11 @@ export default {
   margin-left: 20px
 }
 
-#banner-icon {
-  display: none; /* Hide hamburger menu on desktop */
-}
-
-#banner-small img {
-  padding-top: 10px;
-}
-
 .side-nav-logo {
   width: 75%;
   margin-left: auto;
   margin-right: auto;
-  margin-bottom: 10px;
+  height: 90px;
 }
 
 .side-nav-items {
