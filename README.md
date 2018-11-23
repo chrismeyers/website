178dc755ea188b7ed4c8b96742e699c8bf18438e
Chris Meyers <cm.02.93@gmail.com>
2018-11-23 10:51:44 -0500

    added vue-progressbar to display of the loading state of a page.

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ This is the codebase for my personal website currently located at <https://chris
   + [Vue.js](https://vuejs.org/) as a fontend Javascript framework.
     * [vue-image-lightbox](https://github.com/pexea12/vue-image-lightbox) to make viewing images a lot more pleasant.
     * [vue-js-modal](https://github.com/euvl/vue-js-modal) to raise modals and dialogs.
+    * [vue-progressbar](https://github.com/hilongjw/vue-progressbar) to display of the loading state of a page.
   * [Aqueduct](https://aqueduct.io/) as a backend API framework.
   * [hamburgers](https://jonsuh.com/hamburgers/) to animate the hamburger menu icon.
--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Navigation/>
     <v-dialog/>
+    <vue-progress-bar></vue-progress-bar>
     <router-view/>
     <Footer/>
   </div>
@@ -18,7 +19,32 @@ export default {
     // eslint-disable-next-line
     Footer
   },
+  created() {
+    // [App.vue specific] When App.vue is first loaded start the progress bar
+    this.$Progress.start()
+    // hook the progress bar to start before we move router-view
+    this.$router.beforeEach((to, from, next) => {
+      // does the page we want to go to have a meta.progress object
+      if (to.meta.progress !== undefined) {
+        let meta = to.meta.progress
+        // parse meta tags
+        this.$Progress.parseMeta(meta)
+      }
+      // start the progress bar
+      this.$Progress.start()
+      // continue to next page
+      next()
+    })
+    // hook the progress bar to finish after we've finished moving router-view
+    this.$router.afterEach(() => {
+      // finish the progress bar
+      this.$Progress.finish()
+    })
+  },
   mounted() {
+    // [App.vue specific] When App.vue is finish loading finish the progress bar
+    this.$Progress.finish()
+
     if(this.$isIE) {
       this.showIEDialog()
     }
