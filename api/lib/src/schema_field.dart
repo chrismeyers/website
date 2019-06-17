a83dd55c245cf4b733a970430a74649101ce53b7
Chris Meyers <cm.02.93@gmail.com>
2019-06-16 21:27:29 -0600

    added dynamic schema building classes.

---
+++ api/lib/src/schema_field.dart
@@ -0,0 +1,16 @@
+class SchemaField {
+    const SchemaField(
+      {String tag = "input",
+      String type = "text",
+      bool multiple = false,
+      bool required = true})
+      : tag = tag,
+        type = type,
+        multiple = multiple,
+        required = required;
+
+    final String tag;
+    final String type;
+    final bool multiple;
+    final bool required;
+}
---
+++ api/lib/src/schema_maker.dart
@@ -0,0 +1,34 @@
+import "dart:mirrors";
+import 'package:api/src/schema_field.dart';
+
+class SchemaMaker {
+  static List<Map<String, dynamic>> build(Type type) {
+    final List<Map<String, dynamic>> schema = [];
+
+    reflectClass(type).declarations.forEach((symbol, decl) {
+      decl.metadata.forEach((inst) {
+        if(inst.reflectee.runtimeType == SchemaField) {
+          final Map<String, dynamic> item = {
+            "field": MirrorSystem.getName(symbol),
+            "tag": inst.getField(#tag).reflectee
+          };
+
+          // Setting a field to null will prevent adding the field.
+          if(inst.getField(#type).reflectee != null) {
+            item["type"] = inst.getField(#type).reflectee;
+          }
+          if(inst.getField(#multiple).reflectee != null) {
+            item["multiple"] = inst.getField(#multiple).reflectee;
+          }
+          if(inst.getField(#required).reflectee != null) {
+            item["required"] = inst.getField(#required).reflectee;
+          }
+
+          schema.add(item);
+        }
+      });
+    });
+
+    return schema;
+  }
+}
