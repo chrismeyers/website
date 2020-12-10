3cdf864e4f65bfb2d347caa7460d5f9d6d416caa
Chris Meyers <chris@chrismeyers.info>
2020-12-10 15:23:52 -0700

    added missing semicolon

--- web/src/store/constants.js
+++ web/src/store/constants.js
@@ -1,7 +1,7 @@
 // See: https://stackoverflow.com/a/9851769/7159369
 // prettier-ignore
 /* eslint-disable-next-line */
-export const IS_IE = /*@cc_on!@*/false || !!document.documentMode
+export const IS_IE = /*@cc_on!@*/false || !!document.documentMode;
 export const THEMES = { LIGHT: 'light', DARK: 'dark' };
 export const API_TOKEN_KEY = 'chrismeyers_info_apiToken';
 export const MOBILE_BREAKPOINT = 970; // px
