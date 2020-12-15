e47d9b8ba32d7fd4bb47135f9b1fcea1e14c7e98
Chris Meyers <chris@chrismeyers.info>
2020-12-15 12:25:56 -0700

    split values into variables in AboutPage unit test

--- web/tests/unit/AboutPage.spec.js
+++ web/tests/unit/AboutPage.spec.js
@@ -18,28 +18,29 @@ describe('AboutPage', () => {
   });
 
   it('displays current job if currently employed', async () => {
+    const employed = true;
+    const company = 'Somewhere';
+    const url = 'https://company.com';
+    const title = 'Wizard';
+    const dates = ['Jan. 1234', 'Present'];
+
     await wrapper.setData({
-      mostRecentJob: {
-        employed: true,
-        company: 'Somewhere',
-        url: 'https://company.com',
-        title: 'Wizard',
-        dates: ['Jan. 1234', 'Present'],
-      },
-      employed: true,
+      mostRecentJob: { employed, company, url, title, dates },
+      employed,
     });
 
     const el = wrapper.findComponent({ ref: 'employment' });
 
     expect(el.exists()).toBe(true);
-    expect(el.text()).toContain('Wizard');
-    expect(el.text()).toContain('Somewhere');
-    expect(el.html()).toContain('href="https://company.com"');
+    expect(el.text()).toContain(title);
+    expect(el.text()).toContain(company);
+    expect(el.html()).toContain(`href="${url}"`);
   });
 
   it('displays language experience', async () => {
     const desktop = ['Language 1', 'Language 2'];
     const web = ['Language 3', 'Language 4'];
+
     await wrapper.setData({ languages: { desktop, web } });
 
     let el = wrapper.findComponent({ ref: 'desktop-languages' });
