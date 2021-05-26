c3ae001c5d3fcb3ede6095138e8554498d2aaf3a
Chris Meyers <chris@chrismeyers.net>
2021-05-25 21:11:50 -0600

    installed eslint prettier config and plugin

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['react-app', 'react-app/jest'],
+  extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
   rules: {
     'no-console': 'warn',
     'prefer-const': 'warn',
