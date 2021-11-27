58dab09198ecbbe1c152c4c05f71becab7c00f39
Chris Meyers <chris@chrismeyers.net>
2021-11-26 22:52:30 -0700

    updated build page unit tests

--- packages/web/src/__tests__/unit/BuildPage.test.js
+++ packages/web/src/__tests__/unit/BuildPage.test.js
@@ -34,58 +34,15 @@ describe('BuildPage', () => {
     const id = 1;
     const displayDate = 'Today - Built for Someone';
     const cpu = 'Intel 1000000K CPU @ 99.9GHz';
-    const cool = 'Cold Cooling';
+    const cool = null;
     const mobo = 'Motherboard with a lot of LEDs';
     const ram = '999GB Fast Memory 9999MHz';
-    const hdd = 'Fast HDD 9999GB @ 9999rpm';
-    const ssd = 'Faster SSD 9999GB';
+    const hdd = null;
+    const ssd = null;
     const gpu = 'The best GPU 99GB';
     const image = null;
-
-    nock(process.env.REACT_APP_API_BASE_URL)
-      .defaultReplyHeaders({
-        'access-control-allow-origin': '*',
-      })
-      .get('/builds/1')
-      .once()
-      .reply(200, {
-        id,
-        displayDate,
-        cpu,
-        mobo,
-        ram,
-        hdd,
-        gpu,
-        image,
-      });
-
-    render(<BuildPage />);
-
-    await waitFor(() => {
-      expect(screen.getByText(displayDate)).toBeInTheDocument();
-    });
-
-    expect(screen.getByText(cpu)).toBeInTheDocument();
-    await expect(screen.findByText(cool)).rejects.toThrow();
-    expect(screen.getByText(mobo)).toBeInTheDocument();
-    expect(screen.getByText(ram)).toBeInTheDocument();
-    expect(screen.getByText(hdd)).toBeInTheDocument();
-    await expect(screen.findByText(ssd)).rejects.toThrow();
-    expect(screen.getByText(gpu)).toBeInTheDocument();
-    await expect(screen.findByTitle('Click to enlarge')).rejects.toThrow();
-  });
-
-  it('displays build details without image correctly', async () => {
-    const id = 1;
-    const displayDate = 'Today - Built for Someone';
-    const cpu = 'Intel 1000000K CPU @ 99.9GHz';
-    const cool = 'Cold Cooling';
-    const mobo = 'Motherboard with a lot of LEDs';
-    const ram = '999GB Fast Memory 9999MHz';
-    const hdd = 'Fast HDD 9999GB @ 9999rpm';
-    const ssd = 'Faster SSD 9999GB';
-    const gpu = 'The best GPU 99GB';
-    const image = null;
+    const pcCase = 'Shiny Case';
+    const psu = 'Powerful PSU';
 
     nock(process.env.REACT_APP_API_BASE_URL)
       .defaultReplyHeaders({
@@ -104,6 +61,61 @@ describe('BuildPage', () => {
         ssd,
         gpu,
         image,
+        case: pcCase,
+        psu,
+      });
+
+    render(<BuildPage />);
+
+    await waitFor(() => {
+      expect(screen.getByText(displayDate)).toBeInTheDocument();
+    });
+
+    expect(screen.getByText(cpu)).toBeInTheDocument();
+    await expect(screen.findByText(cool)).rejects.toThrow();
+    expect(screen.getByText(mobo)).toBeInTheDocument();
+    expect(screen.getByText(ram)).toBeInTheDocument();
+    await expect(screen.findByText(hdd)).rejects.toThrow();
+    await expect(screen.findByText(ssd)).rejects.toThrow();
+    expect(screen.getByText(gpu)).toBeInTheDocument();
+    await expect(screen.findByTitle('Click to enlarge')).rejects.toThrow();
+    expect(screen.getByText(pcCase)).toBeInTheDocument();
+    expect(screen.getByText(psu)).toBeInTheDocument();
+  });
+
+  it('displays build details without image correctly', async () => {
+    const id = 1;
+    const displayDate = 'Today - Built for Someone';
+    const cpu = 'Intel 1000000K CPU @ 99.9GHz';
+    const cool = 'Cold Cooling';
+    const mobo = 'Motherboard with a lot of LEDs';
+    const ram = '999GB Fast Memory 9999MHz';
+    const hdd = 'Fast HDD 9999GB @ 9999rpm';
+    const ssd = 'Faster SSD 9999GB';
+    const gpu = 'The best GPU 99GB';
+    const image = null;
+    const pcCase = 'Shiny Case';
+    const psu = 'Powerful PSU';
+
+    nock(process.env.REACT_APP_API_BASE_URL)
+      .defaultReplyHeaders({
+        'access-control-allow-origin': '*',
+      })
+      .get('/builds/1')
+      .once()
+      .reply(200, {
+        id,
+        displayDate,
+        cpu,
+        cool,
+        mobo,
+        ram,
+        hdd,
+        ssd,
+        gpu,
+        image,
+        case: pcCase,
+        psu,
       });
 
     render(<BuildPage />);
@@ -120,6 +132,63 @@ describe('BuildPage', () => {
     expect(screen.getByText(ssd)).toBeInTheDocument();
     expect(screen.getByText(gpu)).toBeInTheDocument();
     await expect(screen.findByTitle('Click to enlarge')).rejects.toThrow();
+    expect(screen.getByText(pcCase)).toBeInTheDocument();
+    expect(screen.getByText(psu)).toBeInTheDocument();
+  });
+
+  it('displays build details with multiple hard drives correctly', async () => {
+    const id = 1;
+    const displayDate = 'Today - Built for Someone';
+    const cpu = 'Intel 1000000K CPU @ 99.9GHz';
+    const cool = 'Cold Cooling';
+    const mobo = 'Motherboard with a lot of LEDs';
+    const ram = '999GB Fast Memory 9999MHz';
+    const hdd = 'Fast HDD 9999GB @ 9999rpm, Slow HDD 99GB @ 99rpm';
+    const ssd = 'Faster SSD 9999GB';
+    const gpu = 'The best GPU 99GB';
+    const image = null;
+    const pcCase = 'Shiny Case';
+    const psu = 'Powerful PSU';
+
+    nock(process.env.REACT_APP_API_BASE_URL)
+      .defaultReplyHeaders({
+        'access-control-allow-origin': '*',
+      })
+      .get('/builds/1')
+      .once()
+      .reply(200, {
+        id,
+        displayDate,
+        cpu,
+        cool,
+        mobo,
+        ram,
+        hdd,
+        ssd,
+        gpu,
+        image,
+        case: pcCase,
+        psu,
+      });
+
+    render(<BuildPage />);
+
+    await waitFor(() => {
+      expect(screen.getByText(displayDate)).toBeInTheDocument();
+    });
+
+    expect(screen.getByText(cpu)).toBeInTheDocument();
+    expect(screen.getByText(cool)).toBeInTheDocument();
+    expect(screen.getByText(mobo)).toBeInTheDocument();
+    expect(screen.getByText(ram)).toBeInTheDocument();
+    const hdds = hdd.split(',').map((hdd) => hdd.trim());
+    expect(screen.getByText(hdds[0])).toBeInTheDocument();
+    expect(screen.getByText(hdds[1])).toBeInTheDocument();
+    expect(screen.getByText(ssd)).toBeInTheDocument();
+    expect(screen.getByText(gpu)).toBeInTheDocument();
+    await expect(screen.findByTitle('Click to enlarge')).rejects.toThrow();
+    expect(screen.getByText(pcCase)).toBeInTheDocument();
+    expect(screen.getByText(psu)).toBeInTheDocument();
   });
 
   it('displays build details with image correctly', async () => {
@@ -140,6 +209,8 @@ describe('BuildPage', () => {
       pos: 1,
       orient: 'land',
     };
+    const pcCase = 'Shiny Case';
+    const psu = 'Powerful PSU';
 
     nock(process.env.REACT_APP_API_BASE_URL)
       .defaultReplyHeaders({
@@ -158,6 +229,8 @@ describe('BuildPage', () => {
         ssd,
         gpu,
         image,
+        case: pcCase,
+        psu,
       });
 
     render(<BuildPage />);
@@ -181,5 +254,7 @@ describe('BuildPage', () => {
       'href',
       image.path,
     );
+    expect(screen.getByText(pcCase)).toBeInTheDocument();
+    expect(screen.getByText(psu)).toBeInTheDocument();
   });
 });
