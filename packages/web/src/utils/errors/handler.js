8dfb36bd4b36ae97f1e7d5e7847e3a6d2d1ef422
Chris Meyers <chris@chrismeyers.net>
2022-05-18 19:42:10 -0600

    Fixed axios error handler
    Changes to error handling were made in axios@0.27.0 that broke the
    existing error handler since both request and response properties always
    exist on the AxiosError

--- packages/web/src/utils/errors/handler.js
+++ packages/web/src/utils/errors/handler.js
@@ -1,22 +1,19 @@
+import { AxiosError } from 'axios';
 import ConnectionError from './types/connection';
 import ResponseError from './types/response';
 
 export function handleAxiosError(error) {
-  if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    return new ResponseError(
-      error.response.data.error,
-      error.response.data.message,
-      error.response.data.statusCode,
-    );
-  } else if (error.request) {
-    // The request was made but no response was received
-    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-    // http.ClientRequest in node.js
-    return new ConnectionError();
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    return error;
+  switch (error.code) {
+    case AxiosError.ERR_NETWORK:
+      return new ConnectionError();
+    case AxiosError.ERR_BAD_REQUEST:
+    case AxiosError.ERR_BAD_RESPONSE:
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      const { data } = error.response;
+      return new ResponseError(data?.error, data?.message, data?.statusCode);
+    default:
+      // Something happened in setting up the request that triggered an Error
+      return error;
   }
 }
