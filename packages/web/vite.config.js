dd05b477d2c22bda969d27fec3f51a398dc92759
Chris Meyers <chris@chrismeyers.net>
2022-06-29 22:48:04 -0600

    Simplified svgr vite plugin

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +10,5 @@ export default defineConfig({
   server: {
     port: '8080',
   },
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
 });
