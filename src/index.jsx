021be13ba9b0bb8b578a84baef883d0189d3e8c6
Chris Meyers <chris@chrismeyers.net>
2022-09-27 00:33:48 -0600

    Removed unused dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Axios from 'axios';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import './assets/vendor/hamburgers/hamburgers.min.css';
-import 'react-loading-skeleton/dist/skeleton.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 import App from './App';
 
-Axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-
 if ('scrollRestoration' in window.history) {
   window.history.scrollRestoration = 'auto';
 }
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 1,
-      retry: (count, error) => {
-        if ([400, 404].includes(error.statusCode)) return false;
-        return count < 3;
-      },
-    },
-  },
-});
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 );
