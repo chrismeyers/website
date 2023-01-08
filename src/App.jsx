abcbd2fde8280e6530e7298604053e7f88980f96
Chris Meyers <chris@chrismeyers.net>
2023-01-08 13:40:12 -0700

    Initial migration to createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
-import FullNav from './components/FullNav';
-import MobileNav from './components/MobileNav';
-import Prompt from './components/Prompt';
-import Footer from './components/Footer';
+import Root from './components/Root';
 import AboutPage from './components/AboutPage';
 import ResumePage from './components/ResumePage';
 import ProjectsPage from './components/ProjectsPage';
@@ -19,36 +15,24 @@ const App = () => {
   const theme = useTheme();
   const { isMobileWidth } = useScreenResize();
 
-  return (
-    <>
-      <BrowserRouter>
-        {isMobileWidth ? <MobileNav theme={theme} /> : <FullNav />}
-        <Routes>
-          <Route path="/" element={<AboutPage />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/projects/:id" element={<ProjectPage />} />
-          <Route path="/builds" element={<BuildsPage />} />
-          <Route path="/builds/:id" element={<BuildPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        {isMobileWidth ? <div /> : <Prompt theme={theme} />}
-      </BrowserRouter>
-      <Footer theme={theme} />
-      <ToastContainer
-        theme={theme.theme}
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root isMobileWidth={isMobileWidth} theme={theme} />,
+      errorElement: <NotFoundPage />,
+      children: [
+        { path: '', element: <AboutPage /> },
+        { path: 'resume', element: <ResumePage /> },
+        { path: 'projects', element: <ProjectsPage /> },
+        { path: 'projects/:id', element: <ProjectPage /> },
+        { path: 'builds', element: <BuildsPage /> },
+        { path: 'builds/:id', element: <BuildPage /> },
+        { path: '*', element: <NotFoundPage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
