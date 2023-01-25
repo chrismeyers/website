28a987a1db62a619e8d2aa9f49c5c22a879ef526
Chris Meyers <chris@chrismeyers.net>
2023-01-24 22:00:33 -0700

    Additional resume request verbiage tweaks

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ export const GITHUB_URL = 'https://github.com/chrismeyers';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/chris-meyers';
 export const MAILTO_HREF = 'mailto:chris@chrismeyers.net';
 export const MAILTO_HREF_MESSAGE = `${MAILTO_HREF}?subject=Message%20from%20chrismeyers.net`;
-export const MAILTO_HREF_RESUME = `${MAILTO_HREF}?subject=Request%20for%20resume%20PDF`;
+export const MAILTO_HREF_RESUME = `${MAILTO_HREF}?subject=Requesting%20copy%20of%20resume%20PDF`;
 export const DEFAULT_DOCUMENT_TITLE =
   'Chris Meyers - Developer, Tech Enthusiast';
 export const LIGHTGALLERY_LICENSE =
