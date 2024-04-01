dbd19ad3847d2d8e5f9e43c6b17b69078b840c72
Chris Meyers <chris@chrismeyers.net>
2024-04-01 16:36:45 -0600

    Updated resume PDF request verbiage

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const GITHUB_URL = 'https://github.com/chrismeyers';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/chris-meyers';
 export const EMAIL_ADDRESS = 'chris@chrismeyers.net';
 export const MAILTO_MESSAGE = `mailto:${EMAIL_ADDRESS}?subject=Message%20from%20chrismeyers.net`;
-export const MAILTO_RESUME = `mailto:${EMAIL_ADDRESS}?subject=Request%20for%20résumé%20PDF`;
+export const MAILTO_RESUME = `mailto:${EMAIL_ADDRESS}?subject=Résumé%20PDF%20request`;
 export const SLOGAN = 'Chris Meyers - Developer, Tech Enthusiast';
 export const LIGHTGALLERY_LICENSE =
   import.meta.env.VITE_LIGHTGALLERY_LICENSE || '0000-0000-000-0000';
